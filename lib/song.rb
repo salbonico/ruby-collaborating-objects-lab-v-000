5df2749f7db534d71b61b2533dc8500a9bf68bda
Steve Albonico <salbonico@gmail.com>
2019-01-18 19:21:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,6 +11,7 @@ def self.new_by_filename(filename)
   array = filename.split(" - ")
   song = Song.new(array[1])
   song.artist = Artist.find_or_create_by_name(array[0])
+  song.artist.add_song(song)
   return song
 end
 
